bf6306d015983e16c1f372edbf3b3520e7b483fb
MalithShehan <shehansandakalum2003@gmail.com>
2024-02-02 17:06:40 +0530

    Add data to Tables

--- src/Applnitializer.java
+++ src/Applnitializer.java
@@ -1,5 +1,28 @@
+import config.SessionFactoryConfig;
+import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Applnitializer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Session session = SessionFactoryConfig
+                .getInstance()
+                .getSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        Customer customer = new Customer();
+        customer.setId(001);
+        customer.setName("Malith");
+        customer.setAddress("Galle");
+        customer.setSalary(52000.00);
+
+        session.save(customer);
+
+        transaction.commit();
+
+        session.close();
+
     }
 }

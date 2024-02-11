2407030e5f23246ab6cf1ad1945d44257b6378a4
MalithShehan <shehansandakalum2003@gmail.com>
2024-02-11 10:44:03 +0530

    Delete

--- src/Applnitializer.java
+++ src/Applnitializer.java
@@ -6,23 +6,70 @@ import org.hibernate.Transaction;
 public class Applnitializer {
     public static void main(String[] args) {
 
-        Session session = SessionFactoryConfig
-                .getInstance()
-                .getSession();
+//        Session session = SessionFactoryConfig
+//                .getInstance()
+//                .getSession();
+//
+//        Transaction transaction = session.beginTransaction();
+//
+//        Customer customer = getCustomer();
+//
+//        session.save(customer);
+//
+//        transaction.
+//                commit();
+//
+//        session.close();
 
-        Transaction transaction = session.beginTransaction();
+        //Get
 
+//        Session getSession =
+//                SessionFactoryConfig.
+//                        getInstance().
+//                        getSession();
+//        Customer existingCustomer =
+//                getSession.
+//                        get(Customer.class, 1);
+//
+//        System.out.println("Existing Customer" +existingCustomer);
+//
+//        getSession.close();
+
+        //Update
+
+//        Session updateSession =
+//                SessionFactoryConfig.
+//                getInstance().
+//                getSession();
+//        Transaction updateTransaction = updateSession.beginTransaction();
+//
+//        Customer existingCustomer = updateSession.get(Customer.class, 1);
+//
+//        existingCustomer.setAddress("Galle");
+//
+//        updateSession.update(existingCustomer);
+//
+//        updateSession.close();
+
+        //Delete
+        Session deleteSession = SessionFactoryConfig.getInstance().getSession();
+
+        Customer existingCus = deleteSession.get(Customer.class,1);
+        existingCus.setAddress("Mathara");
+        Transaction deleteTransaction =
+                deleteSession.beginTransaction();
+
+        deleteSession.delete(existingCus);
+        deleteTransaction.commit();
+
+    }
+
+    private static Customer getCustomer() {
         Customer customer = new Customer();
-        customer.setId(002);
+        customer.setId(001);
         customer.setName("Shehan");
         customer.setAddress("Mathara");
         customer.setSalary(82000.00);
-
-        session.save(customer);
-
-        transaction.commit();
-
-        session.close();
-
+        return customer;
     }
 }
